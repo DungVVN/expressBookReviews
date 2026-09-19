4448f516c4d0b970c568f85cfdd8691cb3537774
Vu Van Ngoc Dung <dungngoc2992005@gmail.com>
2026-09-20 03:29:23 +0700

    Complete Express book reviews assignment

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  const authorization = req.session.authorization;
+  if (!authorization || !authorization.accessToken) {
+    return res.status(401).json({ message: "Please log in first" });
+  }
+
+  jwt.verify(authorization.accessToken, "access", (error, user) => {
+    if (error) {
+      return res.status(403).json({ message: "User not authenticated" });
+    }
+    req.user = user;
+    next();
+  });
 });
  
 const PORT =5000;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -6,38 +7,74 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+  if (isValid(username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+  return res.status(201).json({ message: "User successfully registered" });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = books[req.params.isbn];
+  if (!book) return res.status(404).json({ message: "Book not found" });
+  return res.status(200).json({ [req.params.isbn]: book });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLocaleLowerCase();
+  const matches = Object.fromEntries(Object.entries(books).filter(([, book]) =>
+    book.author.toLocaleLowerCase().includes(author)
+  ));
+  return res.status(200).json(matches);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLocaleLowerCase();
+  const matches = Object.fromEntries(Object.entries(books).filter(([, book]) =>
+    book.title.toLocaleLowerCase().includes(title)
+  ));
+  return res.status(200).json(matches);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = books[req.params.isbn];
+  if (!book) return res.status(404).json({ message: "Book not found" });
+  return res.status(200).json(book.reviews);
 });
 
+// Axios examples for task 11. They are exported so they can be reused by another
+// Node client without duplicating request logic.
+const apiBaseUrl = process.env.BOOK_REVIEWS_API_URL || "http://localhost:5000";
+
+const getAllBooksWithPromise = () => axios.get(apiBaseUrl).then((response) => response.data);
+const getBookByISBNWithPromise = (isbn) =>
+  axios.get(`${apiBaseUrl}/isbn/${encodeURIComponent(isbn)}`).then((response) => response.data);
+
+const getBooksByAuthorWithAsyncAwait = async (author) => {
+  const response = await axios.get(`${apiBaseUrl}/author/${encodeURIComponent(author)}`);
+  return response.data;
+};
+
+const getBooksByTitleWithAsyncAwait = async (title) => {
+  const response = await axios.get(`${apiBaseUrl}/title/${encodeURIComponent(title)}`);
+  return response.data;
+};
+
 module.exports.general = public_users;
+module.exports.getAllBooksWithPromise = getAllBooksWithPromise;
+module.exports.getBookByISBNWithPromise = getBookByISBNWithPromise;
+module.exports.getBooksByAuthorWithAsyncAwait = getBooksByAuthorWithAsyncAwait;
+module.exports.getBooksByTitleWithAsyncAwait = getBooksByTitleWithAsyncAwait;
